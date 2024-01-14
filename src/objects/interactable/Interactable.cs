f6e7cdcef8db590ce763e720ce825c6c723b5cf8
GetLocalPlayer <GetLocalPlayer@gmail.com>
2024-01-14 16:26:32 +0400

    Interactable makes Nime learn spell on autocast. Added fire magic interaction at the baker's house

--- src/objects/interactable/Interactable.cs
+++ src/objects/interactable/Interactable.cs
@@ -167,6 +167,7 @@ public partial class Interactable : Node2D
 	protected virtual void OnSpellRevealed()
 	{
 		GD.Print("Spell revealed");
+		GetTree().CallGroup("Player", "InteractableSpellRevealed", SpellName);
 		OnSpellCast(SpellName);
 	}
 
